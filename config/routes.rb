8110204b5bc8deb3ffab172cfef6003b0dd9374a
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-05-16 12:17:37 +0000

    ルート作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,32 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  
+ scope module: :public do 
+ root to:'homes#top'
+ get '/about' => 'homes#about'
+ resources :items , only: [:index, :show]
+ get 'customers/my_page' => 'customers#show'
+ get 'customers/information/edit' => 'customers#edit'
+ patch 'customers/information' => 'customers#update'
+ get 'customers/confirm' => 'customers#confirm'
+ patch 'customers/withdraw' => 'customers#withdraw'
+ resources :cart_items , only: [:index, :update, :destroy, :create]
+ delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+ resources :orders , only: [:new, :create, :index, :show]
+ post 'orders/confirm' => 'orders#confirm'
+ get 'orders/finish' => 'orders#finish'
+ resources :addresses , only: [:index, :edit, :create, :update, :destroy]
+ end
+ 
+ 
+ namespace :admin do
+ get '/' => 'homes#top'
+ resources :items , only: [:index, :new, :create, :show,:edit, :update]
+ resources :genres , only: [:index, :create, :edit, :update]
+ resources :customers , only: [:index, :show, :edit, :update]
+ resources :orders , only: [:show, :update]
+ resources :order_details , only: [:update]
+ end
 end
