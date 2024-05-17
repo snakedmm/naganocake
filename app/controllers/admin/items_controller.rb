2fa0409f3b51412917e763b8e6b7b80844017fc2
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-05-17 01:41:12 +0000

    商品登録作成(ActiveStorage導入前)

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,4 +4,20 @@ class Admin::ItemsController < ApplicationController
     @item = Item.new
   end
   
+  def create
+    item = Item.new(item_params)
+    if item.save
+      redirect_to admin_items_path
+    else
+      render :new
+    end
+  end
+  
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :status)
+  end
+  
 end
