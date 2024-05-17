410135efddc198c860e4d7efe2713b6111b451a5
norikkun <161901725+norikkun@users.noreply.github.com>
2024-05-17 09:10:18 +0000

    サインアップ　サインイン　ログアウト機能第一形態

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,3 +1,7 @@
 class Public::HomesController < ApplicationController
   before_action :authenticate_customer!
+  
+  def top
+    
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,14 +2,25 @@ class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
  has_many :cart_items, dependent: :destroy
  has_many :items,through: :cart_item, dependent: :destroy
  has_many :orders, dependent: :destroy
  has_many :addresses, dependent: :destroy
 
-
+ validates :last_name, presence: true
+ validates :first_name, presence: true
+ validates :last_name_kana, presence: true
+ validates :last_name_kana, presence: true
+ validates :first_name_kana, presence: true
+ validates :email, presence: true
+ validates :post_code, presence: true
+ validates :address, presence: true
+ validates :phone_number, presence: true
+ validates :password, presence: true
+ validates :password_confirmation, presence: true
+ 
 
 
 end
