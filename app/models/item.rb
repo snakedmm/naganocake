37752f9ad8bbab961de393715e77cb1ffd96e5db
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-05-17 03:29:47 +0000

    activestrageとテーブルの紐づけ

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,11 @@ class Item < ApplicationRecord
   
   has_one_attached :image
   
+  has_many :cart_items, dependent: :destroy
+  has_many :customers,through: :cart_items , dependent: :destroy
+  has_many :order_details, dependent: :destroy
+  has_many :orders,through: :order_details , dependent: :destroy
+  
+  belongs_to :genre
   
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+  has_many :order_details, dependent: :destroy
+  has_many :items,through: :order_details, dependent: :destroy
+  
+  belongs_to :customer
 end
