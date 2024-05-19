c8e842bc8cb429a7f60f8833d0481bb07f7d8a44
atijuF <162565598+atijuF@users.noreply.github.com>
2024-05-19 11:08:32 +0000

    カート機能大枠作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,7 +1,12 @@
 class Public::CartItemsController < ApplicationController
+  
+  def index
+    @cart_item = CartItem.all
+  end
+  
   def create
     cart_item = CartItem.new(cart_item_params)
-    cart_item.end_customer_id = current_end_customer.id
+    cart_item.customer_id = current_customer.id 
     cart_item.item_id = cart_item_params[:item_id]
     if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
       cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
@@ -13,16 +18,21 @@ class Public::CartItemsController < ApplicationController
       redirect_to cart_items_path
     end
   end
-
-  private
-  def cart_item_params
-    params.require(:cart_item).permit(:item_id, :amount)
+  
+  def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.update(cart_item_params)
+    redirect_back(fallback_location: root_path)
   end
 
-  
   def destroy_all
     current_customer.cart_items.destroy_all
     redirect_to cart_items_path, notice: 'カートを空にしました'
+  end
+  
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
   
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,8 @@ class Item < ApplicationRecord
   
   belongs_to :genre
   
+  def with_tax_price
+    (price * 1.1).floor
+  end
+  
 end
