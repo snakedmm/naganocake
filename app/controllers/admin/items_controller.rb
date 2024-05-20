07869ad89b01cd208794b124b54089a91e60fc87
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-05-20 01:25:10 +0000

    レイアウト修正(ベーシック)

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -38,7 +38,7 @@ class Admin::ItemsController < ApplicationController
   private
   
   def item_params
-    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :status)
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :status, :is_active)
   end
   
 end
