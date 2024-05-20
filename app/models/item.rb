e389c11f2116ea81bd95f4a9c28ab07566aa493f
norikkun <161901725+norikkun@users.noreply.github.com>
2024-05-20 15:38:22 +0000

    コンフリクト解消

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   
   has_one_attached :image
+  enum status: { on_sale: 0, off_sale: 1 }
   
   has_many :cart_items, dependent: :destroy
   has_many :customers,through: :cart_items , dependent: :destroy
