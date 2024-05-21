0b0fb15e8694f142e5b401b899c2a752636581e0
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-05-21 06:12:15 +0000

    ジャンル検索結果_ジャンル名一覧にタイトル変更

--- app/controllers/public/genre_searches_controller.rb
+++ app/controllers/public/genre_searches_controller.rb
@@ -3,6 +3,7 @@ class Public::GenreSearchesController < ApplicationController
   def search
     @genre_id = params[:genre_id]
     @items = Item.where(is_active: true, genre_id: @genre_id)
+    @genre = Genre.find(params[:genre_id])
     @genres = Genre.all
   end
 end
