3b7cf62336876132cc7e7c759e9f023daa46d3ad
norikkun <161901725+norikkun@users.noreply.github.com>
2024-05-21 08:41:29 +0000

    レイアウト修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,16 +9,16 @@ class Customer < ApplicationRecord
  has_many :orders, dependent: :destroy
  has_many :addresses, dependent: :destroy
 
- validates :last_name, presence: true
- validates :first_name, presence: true
- validates :last_name_kana, presence: true
- validates :first_name_kana, presence: true
  validates :email, presence: true
  validates :post_code, presence: true
  validates :address, presence: true
  validates :phone_number, presence: true
  validates :password, presence: true, length: { minimum: 6 }, on: :create # ユーザー新規登録時のみバリデーションを実行 
  validates :password_confirmation, presence: true, length: { minimum: 6 }, on: :create # ユーザー新規登録時のみバリデーションを実行 
+ 
+ validates :last_name, :first_name, :last_name_kana, :first_name_kana, presence: { message: 'は必須項目です。' }
+ validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力して下さい。' }
+ validates :last_name_kana, :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }
  
  # is_activeがtrueならfalseを返すようにしている
   def active_for_authentication?
