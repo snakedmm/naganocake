5ccfc9c05f563f2eb788bb1e9b2a4a44ca02232f
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-05-22 02:50:43 +0000

    管理者と会員のorderコントローラー修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -57,7 +57,7 @@ class Public::OrdersController < ApplicationController
   
   def show
     @order = Order.find(params[:id])
-    @order_details = OrderDetail.where(order_id: @order.id)
+    
   end
 
   private
