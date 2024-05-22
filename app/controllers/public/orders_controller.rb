f5f1edf9b6e6e375196bfe374a78c37a73ffec00
atijuF <162565598+atijuF@users.noreply.github.com>
2024-05-22 11:15:34 +0000

    customer用製作ステータス完成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -53,7 +53,7 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
-    @orders = current_customer.orders.all
+    @orders = current_customer.orders
   end
   
   def show
