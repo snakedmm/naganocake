24d476838df0762518da6e102139ec7472479e25
norikkun <161901725+norikkun@users.noreply.github.com>
2024-05-23 02:41:24 +0000

    レイアウト全般

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,24 +1,14 @@
 class Item < ApplicationRecord
-  
+
   has_one_attached :image
-  enum status: { on_sale: 0, off_sale: 1 }
+  enum sale_status: { on_sale: 0, off_sale: 1 }
   
   has_many :cart_items, dependent: :destroy
-  has_many :customers,through: :cart_items , dependent: :destroy
+  has_many :customers, through: :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
-  has_many :orders,through: :order_details , dependent: :destroy
+  has_many :orders, through: :order_details, dependent: :destroy
   
   belongs_to :genre
-  
-  enum status: { on_sale: 0, off_sale: 1 }
-  def add_tax_price
-    (self.price * 1.10).round
-  end
-  
-  def self.active_items_count
-    where(is_active: true).count
-
-  end
   
   def add_tax_price
     (self.price * 1.10).round
